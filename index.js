018d6df514c369465153431db351e179dfd0522e
Khatri, Amol <amol.khatri@sincrodigital.com>
2024-01-04 15:10:19 +0530

    Fourth commit

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public"), {
+    "setHeaders": (res, path, stat) => {
+        res.set("cache-control", "s-maxage=60")
+    }
+});
 
 app.listen(3000, () => {
     console.log("Running server at 3000");
 })
+
